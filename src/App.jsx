ba9177325a350ba53ad954820adaf37b8f92fb52
Kaue Siqueira Reis <kauesiqueira_@hotmail.com>
2023-02-14 20:54:56 -0300

    parte lógica de um Contador

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react'
+
+const COUNTDOWN_INITIAL_TIME_IN_SECONDS = 25 * 60 // 25 minutes
+
+function App() {
+  const [secondsAmount, setSecondsAmount] = useState(COUNTDOWN_INITIAL_TIME_IN_SECONDS)
+
+  useEffect(() => {
+    if (secondsAmount === 0) { 
+      alert('Chegou em Zero!')
+      return;
+    }
+
+      setTimeout(() => {
+        setSecondsAmount(state => state - 1 );
+      }, 1000)
+  }, [secondsAmount]);
+  
+  const minutes = Math.floor(secondsAmount / 60);
+  const seconds = secondsAmount % 60;
+
+  return (
+   <div>
+    <span>{String(minutes).padStart(2, '0')}</span>
+    <span>:</span>
+    <span>{String(seconds).padStart(2, '0')}</span>
+   </div>
+  )
+}
+
+export default App
